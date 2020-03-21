d20a6ce87cec02d96efa886bbacb4ebbb52371c0
LiuChaoqun <chaoqunmail@126.com>
2020-03-21 23:00:41 +0800

    start
    start

---
+++ CQLinedTextView/CQLinedTextView/CQLinedTextView.h
@@ -0,0 +1,17 @@
+//
+//  CQLinedTextView.h
+//  CQLinedTextView
+//
+//  Created by 刘超群 on 2020/3/21.
+//  Copyright © 2020 chaoqun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CQLinedTextView : UITextView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CQLinedTextView/CQLinedTextView/CQLinedTextView.m
@@ -0,0 +1,21 @@
+//
+//  CQLinedTextView.m
+//  CQLinedTextView
+//
+//  Created by 刘超群 on 2020/3/21.
+//  Copyright © 2020 chaoqun. All rights reserved.
+//
+
+#import "CQLinedTextView.h"
+
+@implementation CQLinedTextView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
